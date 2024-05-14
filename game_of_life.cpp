fa9498898bbea63a22d22fc9faea5433d96996bd
AmmaraGillani <ammara.gillani79@gmail.com>
2024-05-14 10:28:57 +0500

    complete version

--- game_of_life.cpp
+++ game_of_life.cpp
@@ -16,20 +16,20 @@ struct coordinate {
 };
 
 void start();
-void read_input_file(string file_name, int grid[GRID_X][GRID_Y], coordinate* sec, int& last, int& generation);
+void read_input_file(string file_name, coordinate sec[], int& last, int& generation);
 string get_user_input();
 void display(int grid[GRID_X][GRID_Y]);
 void display_coordinates(coordinate coordinates[], int count);
-void find_dead_neihbor(int grid[GRID_X][GRID_Y], coordinate target, coordinate* dead, int & count);
-void insert_dead(int grid[GRID_X][GRID_Y], int x, int y, coordinate* dead, int & count);
-int is_exist(coordinate* arr, int count, int x, int y);
+void find_dead_neihbor(int grid[GRID_X][GRID_Y], coordinate target, coordinate nei[], int & count);
+void insert_dead(int grid[GRID_X][GRID_Y], int x, int y, coordinate nei[], int & count);
+int is_exist(coordinate arr[], int count, int x, int y);
 int is_neighbor(int x1, int y1, int x2, int y2);
 int count_neighbors(int grid[GRID_X][GRID_Y], coordinate target);
-vector<coordinate> apply_rule_birth(int grid[GRID_X][GRID_Y], coordinate *dead, int dead_counter);
-vector<coordinate> apply_rule_survival(int grid[GRID_X][GRID_Y], coordinate *sec, int last);
-vector<coordinate> apply_rule_loneliness(int grid[GRID_X][GRID_Y], coordinate *dead, int dead_counter);
-vector<coordinate> apply_rule_overcrowding(int grid[GRID_X][GRID_Y], coordinate *dead, int dead_counter);
-void apply_rules (int grid[GRID_X][GRID_Y], coordinate* sec, int last, coordinate dead[],int &dead_counter);
+vector<coordinate> apply_rule_birth(int grid[GRID_X][GRID_Y], coordinate nei[], int nei_size);
+vector<coordinate> apply_rule_survival(int grid[GRID_X][GRID_Y], coordinate sec[], int last);
+vector<coordinate> apply_rule_loneliness(int grid[GRID_X][GRID_Y], coordinate nei[], int nei_size);
+vector<coordinate> apply_rule_overcrowding(int grid[GRID_X][GRID_Y], coordinate nei[], int nei_size);
+void apply_rules (int grid[GRID_X][GRID_Y], coordinate sec[], int last, coordinate nei[],int &nei_size);
 int delete_element(coordinate arr[], int &size, coordinate x);
 void delete_elements(vector<coordinate> delete_items, coordinate arr[], int &size);
 void add_element(coordinate arr[], int &size, vector<coordinate>new_items);
@@ -37,6 +37,7 @@ void display_coordinate_count(int grid[GRID_X][GRID_Y], coordinate cord[], int s
 void simulate_generation(int grid[GRID_X][GRID_Y], coordinate sec[], int &last, coordinate nei[], int &nei_size, int generations);
 void populated_grid(int grid[GRID_X][GRID_Y], coordinate sec[], int last);
 void fill_nei(int grid[GRID_X][GRID_Y], coordinate sec[], int last, coordinate nei[], int &nei_size);
+void write_output_file(coordinate sec[], int& last, int & generation);
 
 int main() {
     start();
@@ -51,9 +52,8 @@ void start() {
     int nei_size = 0;
     int generation = 0;
     
-    // string file_name = get_user_input();
-    string file_name = "input2.txt";
-    read_input_file(file_name, grid, sec, last, generation);
+    string file_name = get_user_input();
+    read_input_file(file_name, sec, last, generation);
     populated_grid(grid, sec, last);
     fill_nei(grid, sec, last, nei, nei_size);
     cout << "Display secondary array" << endl;
@@ -63,6 +63,7 @@ void start() {
     cout << "Display populated grid" << endl;
     display(grid);
     simulate_generation(grid, sec, last, nei, nei_size, generation);
+    write_output_file(sec,last,generation);
      
 }
 
@@ -76,7 +77,7 @@ string get_user_input() {
 }
 
 // read input from file
-void read_input_file(string file_name, int grid[GRID_X][GRID_Y], coordinate* sec, int& last, int & generation) {
+void read_input_file(string file_name, coordinate sec[], int& last, int & generation) {
     ifstream file;
     int number_of_cell;
     file.open(file_name.c_str());
@@ -89,7 +90,6 @@ void read_input_file(string file_name, int grid[GRID_X][GRID_Y], coordinate* sec
             coordinate alive;
             file >> x;
             file >> y;
-            //grid[x][y] = 1;
             alive.x = x;
             alive.y = y;
             sec[i] = alive;
@@ -120,27 +120,27 @@ void display_coordinates(coordinate coordinates[], int count) {
 }
 
 // function for finding dead neighborhood
-void find_dead_neihbor(int grid[GRID_X][GRID_Y], coordinate target, coordinate* dead, int & count) {
-    insert_dead(grid, target.x - 1, target.y - 1, dead, count);
-    insert_dead(grid, target.x, target.y - 1, dead, count);
-    insert_dead(grid, target.x + 1, target.y - 1, dead, count);
-    insert_dead(grid, target.x - 1, target.y, dead, count);
-    insert_dead(grid, target.x + 1, target.y, dead, count);
-    insert_dead(grid, target.x - 1, target.y + 1, dead, count);
-    insert_dead(grid, target.x, target.y + 1, dead, count);
-    insert_dead(grid, target.x + 1, target.y + 1, dead, count);
+void find_dead_neihbor(int grid[GRID_X][GRID_Y], coordinate target, coordinate nei[], int & count) {
+    insert_dead(grid, target.x - 1, target.y - 1, nei, count);
+    insert_dead(grid, target.x, target.y - 1, nei, count);
+    insert_dead(grid, target.x + 1, target.y - 1, nei, count);
+    insert_dead(grid, target.x - 1, target.y, nei, count);
+    insert_dead(grid, target.x + 1, target.y, nei, count);
+    insert_dead(grid, target.x - 1, target.y + 1, nei, count);
+    insert_dead(grid, target.x, target.y + 1, nei, count);
+    insert_dead(grid, target.x + 1, target.y + 1, nei, count);
 }
 
-void insert_dead(int grid[GRID_X][GRID_Y], int x, int y, coordinate* dead, int & count) {
-    if (grid[x][y] == 0 && x >= 0 && y >= 0 && is_exist(dead, count, x, y) == 1) {
+void insert_dead(int grid[GRID_X][GRID_Y], int x, int y, coordinate nei[], int & count) {
+    if (grid[x][y] == 0 && x >= 0 && y >= 0 && is_exist(nei, count, x, y) == 1) {
         coordinate die;
         die.x = x;
         die.y = y;
-        dead[count++] = die;
+        nei[count++] = die;
     }
 }
 
-int is_exist(coordinate* arr, int count, int x, int y) {
+int is_exist(coordinate arr[], int count, int x, int y) {
     for(int i = 0; i < count; i++) {
         if(arr[i].x == x && arr[i].y == y) {
             return 0;
@@ -176,21 +176,21 @@ int count_neighbors(int grid[GRID_X][GRID_Y], coordinate target) {
     return count;
 }
 
-vector<coordinate> apply_rule_birth(int grid[GRID_X][GRID_Y], coordinate *dead, int dead_counter)
+vector<coordinate> apply_rule_birth(int grid[GRID_X][GRID_Y], coordinate nei[], int nei_size)
 {
     vector<coordinate> birth_result;
-    for(int i = 0; i < dead_counter; i++)
+    for(int i = 0; i < nei_size; i++)
     {
-        int dead_neighbors = count_neighbors(grid, dead[i]);
+        int dead_neighbors = count_neighbors(grid, nei[i]);
         if (dead_neighbors == 3)
         {
-            birth_result.push_back(dead[i]);
+            birth_result.push_back(nei[i]);
             
         }
     }
     return birth_result;
 }
-vector<coordinate> apply_rule_survival(int grid[GRID_X][GRID_Y], coordinate *sec, int last)
+vector<coordinate> apply_rule_survival(int grid[GRID_X][GRID_Y], coordinate sec[], int last)
 {
     vector<coordinate> survival_result;
     for(int i = 0; i < last; i++)
@@ -205,7 +205,7 @@ vector<coordinate> apply_rule_survival(int grid[GRID_X][GRID_Y], coordinate *sec
     }
     return survival_result;
 }
-vector<coordinate> apply_rule_loneliness(int grid[GRID_X][GRID_Y], coordinate *sec, int last)
+vector<coordinate> apply_rule_loneliness(int grid[GRID_X][GRID_Y], coordinate sec[], int last)
 {
     vector<coordinate> loneliness_result;
     for(int i = 0; i < last; i++)
@@ -220,7 +220,7 @@ vector<coordinate> apply_rule_loneliness(int grid[GRID_X][GRID_Y], coordinate *s
     return loneliness_result;
 }
 
-vector<coordinate> apply_rule_overcrowding(int grid[GRID_X][GRID_Y], coordinate *sec, int last)
+vector<coordinate> apply_rule_overcrowding(int grid[GRID_X][GRID_Y], coordinate sec[], int last)
 {
     vector<coordinate> overcrowding_result;
     for(int i = 0; i < last; i++)
@@ -235,7 +235,7 @@ vector<coordinate> apply_rule_overcrowding(int grid[GRID_X][GRID_Y], coordinate 
     return overcrowding_result;
 }
 
-void apply_rules (int grid[GRID_X][GRID_Y], coordinate* sec, int last, coordinate nei[],int &nei_size)
+void apply_rules (int grid[GRID_X][GRID_Y], coordinate sec[], int last, coordinate nei[],int &nei_size)
 {
     vector<coordinate> birth_result = apply_rule_birth (grid, nei, nei_size);
     vector<coordinate> survival_result = apply_rule_survival(grid, sec, last);
@@ -340,4 +340,20 @@ void fill_nei(int grid[GRID_X][GRID_Y], coordinate sec[], int last, coordinate n
     {
         find_dead_neihbor(grid, sec[i], nei, nei_size);
     }
+}
+// write output from file
+void write_output_file(coordinate sec[], int& last, int & generation)
+ {
+    ofstream file;
+    file.open("output.txt");
+
+    if (file.is_open()) {
+        file << generation << "\n";
+        file << last << "\n";
+        for (int i = 0; i < last; i++)
+        {
+            file << sec[i].x << " " << sec[i].y << "\n";
+        }
+    }
+    file.close();
 }
